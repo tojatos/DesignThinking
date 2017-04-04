4b2d1defd5f2811734a4867bee64a92607def106
Yarde <dawidkal66@gmail.com>
2017-04-04 21:00:51 +0200

    egzamin style

---
+++ application/views/Egzamin/index.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<div class="container col-xs-12 col-sm-10 col-md-10 col-lg-9
+				 col-sm-offset-1 col-md-offset-2 col-lg-offset-3">
+				<div>
+			<a href="<?= site_url('Egzamin/1') ?>"><img src="public/images/Egzamin.png" class="img-responsive" alt="Image"></a>
+		</div>
+						<div>
+			<a href="<?= site_url('Egzamin/2') ?>"><img src="public/images/Egzamin.png" class="img-responsive" alt="Image"></a>
+		</div>
+						<div>
+			<a href="<?= site_url('Egzamin/3') ?>"><img src="public/images/Egzamin.png" class="img-responsive" alt="Image"></a>
+		</div>
+						<div>
+			<a href="<?= site_url('Egzamin/4') ?>"><img src="public/images/Egzamin.png" class="img-responsive" alt="Image"></a>
+		</div>
+				<div>
+			<a href="<?= site_url('Egzamin/5') ?>"><img src="public/images/Egzamin.png" class="img-responsive" alt="Image"></a>
+		</div>
+</div>
--- application/views/Kurs/index.php
+++ application/views/Kurs/index.php
@@ -1,17 +1,19 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
-<div class="container">
-		<div class="col-xs-10 col-sm-9 col-md-8 col-lg-7 
-					col-xs-offset-2 col-sm-offset-3 col-md-offset-4 col-lg-offset-5">
-			<a href="<?= site_url('Kurs/4') ?>"><img src="public/images/kurs.png" class="img-responsive" alt="Image"></a>
-		</div>
-		<div class="col-xs-10 col-sm-6 col-md-5 col-lg-4
-					col-xs-offset-2 col-sm-offset-0 col-md-offset-1 col-lg-offset-2">
+<div class="container col-xs-12 col-sm-10 col-md-10 col-lg-9
+				 col-sm-offset-1 col-md-offset-2 col-lg-offset-3">
+				<div>
 			<a href="<?= site_url('Kurs/1') ?>"><img src="public/images/kurs.png" class="img-responsive" alt="Image"></a>
+		</div>
+						<div>
 			<a href="<?= site_url('Kurs/2') ?>"><img src="public/images/kurs.png" class="img-responsive" alt="Image"></a>
 		</div>
-		<div class="col-xs-10 col-sm-6 col-md-5 col-lg-4
-					col-xs-offset-2 col-sm-offset-0 col-md-offset-1 col-lg-offset-2">
+						<div>
 			<a href="<?= site_url('Kurs/3') ?>"><img src="public/images/kurs.png" class="img-responsive" alt="Image"></a>
+		</div>
+						<div>
+			<a href="<?= site_url('Kurs/4') ?>"><img src="public/images/kurs.png" class="img-responsive" alt="Image"></a>
+		</div>
+				<div>
 			<a href="<?= site_url('Kurs/5') ?>"><img src="public/images/kurs.png" class="img-responsive" alt="Image"></a>
 		</div>
 </div>
