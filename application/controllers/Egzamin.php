778a3a4dfb49a38c754ba94d3a3d88ecd270f9f6
tojatos <tojatos@gmail.com>
2017-04-05 20:58:17 +0200

    egzamin refactor

--- application/controllers/Egzamin.php
+++ application/controllers/Egzamin.php
@@ -7,26 +7,29 @@ class Egzamin extends MY_Controller
     {
         try {
             if (!$this->session->is_logged) {
-                throw new Exception('Aby mieć dostęp do egzaminu musisz się <a href="'.site_url("Login").'">zalogować</a>!');
+                throw new Exception('Aby mieć dostęp do egzaminu musisz się <a href="'.site_url('Login').'">zalogować</a>!');
             }
-            $username = $this->session->user_name;
             $view['mainNav'] = $this->loadMainNav();
             if ($id_egzamin == 0) {
                 $view['content'] = $this->loadContent('Egzamin/index');
             } else {
-                $this->load->model("Kurs_model");
-                $this->load->model("User_model");
-                $user_id = $this->User_model->get_user_id($username);
-                $has_finished_kurs = $this->Kurs_model->has_finihed_kurs(['id_kurs' => $id_egzamin, 'id_user' => $user_id]);
-                if(!$has_finished_kurs)
-                {
-                  throw new Exception('Aby mieć dostęp do tego egzaminu musisz ukończyć odpowiadający mu kurs!');
-                }
+                $this->checkFinishState($id_egzamin);
                 $view['content'] = $this->loadContent('Egzamin/egzamin'.$id_egzamin);
             }
             $this->showMainView($view);
         } catch (Exception $e) {
-          $this->showMessage($e->getMessage());
+            $this->showMessage($e->getMessage());
+        }
+    }
+    private function checkFinishState($id_egzamin)
+    {
+        $this->load->model('Kurs_model');
+        $this->load->model('User_model');
+        $username = $this->session->user_name;
+        $user_id = $this->User_model->get_user_id($username);
+        $has_finished_kurs = $this->Kurs_model->has_finihed_kurs(['id_kurs' => $id_egzamin, 'id_user' => $user_id]);
+        if (!$has_finished_kurs) {
+            throw new Exception('Aby mieć dostęp do tego egzaminu musisz ukończyć odpowiadający mu kurs!');
         }
     }
 }
