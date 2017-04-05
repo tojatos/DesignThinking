e4322d6a18af503d227dabe1f4cf8f9a59760ebb
tojatos <tojatos@gmail.com>
2017-04-05 20:13:36 +0200

    Egzamin basics

--- application/config/routes.php
+++ application/config/routes.php
@@ -6,3 +6,4 @@ $route['translate_uri_dashes'] = FALSE;
 $route['404_override'] = 'Main/error404';
 
 $route['Kurs/(:num)'] = 'Kurs/index/$1';
+$route['Egzamin/(:num)'] = 'Egzamin/index/$1';
--- application/controllers/Egzamin.php
+++ application/controllers/Egzamin.php
@@ -3,8 +3,22 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 class Egzamin extends MY_Controller
 {
-    public function index()
+    public function index($id_egzamin = 0)
     {
-        $this->showView('Egzamin/egzamin');
+        try {
+            if (!$this->session->isLogged) {
+                throw new Exception('Aby mieć dostęp do egzaminu musisz się <a href="'.site_url("Login").'">zalogować</a>!');
+            }
+
+            $view['mainNav'] = $this->loadMainNav();
+            if ($id_egzamin == 0) {
+                $view['content'] = $this->loadContent('Egzamin/index');
+            } else {
+                $view['content'] = $this->loadContent('Egzamin/egzamin'.$id_egzamin);
+            }
+            $this->showMainView($view);
+        } catch (Exception $e) {
+          $this->showMessage($e->getMessage());
+        }
     }
 }
