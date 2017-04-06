845a5b112ada0c9bfdd09de1b454bdbdac58699d
tojatos <tojatos@gmail.com>
2017-04-06 17:20:21 +0200

    egzamin backend

--- application/models/Kurs_model.php
+++ application/models/Kurs_model.php
@@ -46,9 +46,22 @@ class Kurs_model extends MY_Model
     {
         $query = $this->db->get_where(PYTANIE_TABLE, ['kurs_id_kurs' => $exam_id]);
         if ($query->result() == null) {
-            throw new Exception('Ten egzamin nie ma pytań! Skontaktuj się z administratorem.');
+            throw new Exception('Egzamin '.$exam_id.' nie ma pytań! Skontaktuj się z administratorem.');
         }
         $pytania = $query->result();
+        foreach ($pytania as $key => $pytanie) {
+            $id_pytanie = $pytanie->id_pytanie;
+            $query = $this->db->get_where(ODPOWIEDZ_TABLE, ['pytanie_id_pytanie' => $id_pytanie]);
+            if ($query->result() == null) {
+                throw new Exception('Pytanie '.$id_pytanie.' nie ma odpowiedzi! Skontaktuj się z administratorem.');
+            }
+            $odpowiedzi = $query->result();
+            foreach ($odpowiedzi as $odpowiedz) {
+                $litera = $odpowiedz->litera;
+                $pytanie->odpowiedzi[$litera] = $odpowiedz->tresc;
+            }
+        }
+
         return $pytania;
     }
 }
--- application/views/Egzamin/egzamin1.php
+++ application/views/Egzamin/egzamin1.php
@@ -1,19 +1,26 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
-<?php $pytania = $exam_content; dump($pytania); ?>
+<?php $pytania = $exam_content; ?>
 Egzamin 1
 <form class="pytanie_form" action="" method="post">
 <?php
-	$first = true;
-	foreach ($pytania as $pytanie): ?>
-	<div class="pytanie" <?= ($first) ? : 'style="display:none"' ?>>
+	$last = count($pytania)-1;
+	foreach ($pytania as $key=>$pytanie): ?>
+	<div class="pytanie<?= ($key==0) ? : ' hidden"' ?>">
 		<h2>Pytanie <?= $pytanie->id_pytanie ?></h2>
 		<p><?= $pytanie->tresc ?></p>
-		<input type="radio" name="pytanie_<?= $pytanie->id_pytanie ?>" value="A">
-		<input type="radio" name="pytanie_<?= $pytanie->id_pytanie ?>" value="B">
-		<input type="radio" name="pytanie_<?= $pytanie->id_pytanie ?>" value="C">
-		<input type="radio" name="pytanie_<?= $pytanie->id_pytanie ?>" value="D">
+		<input type="hidden" name="id_pytanie" value="<?= $pytanie->id_pytanie ?>">
+		<input type="radio" name="pytanie_<?= $pytanie->id_pytanie ?>" value="A"><label><?= $pytanie->odpowiedzi['A'] ?></label>
+		<input type="radio" name="pytanie_<?= $pytanie->id_pytanie ?>" value="B"><label><?= $pytanie->odpowiedzi['B'] ?></label>
+		<input type="radio" name="pytanie_<?= $pytanie->id_pytanie ?>" value="C"><label><?= $pytanie->odpowiedzi['C'] ?></label>
+		<input type="radio" name="pytanie_<?= $pytanie->id_pytanie ?>" value="D"><label><?= $pytanie->odpowiedzi['D'] ?></label>
+		<?php if($key!=$last): ?>
+			<button type="button" class="next">Następne pytanie</button>
+		<?php else: ?>
+			<input type="submit" value="Zakończ egzamin">
+		<?php endif; ?>
 	</div>
-	<button type="button" class="next">Następne pytanie</button>
 	<?php endforeach; ?>
-	<input type="submit" value="Zakończ egzamin">
+	<div class="pytanie hidden">
+
+	</div>
 </form>
