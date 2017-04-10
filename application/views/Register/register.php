bbe70de069bc195ca25b405b49b5d977dcbf49f6
tojatos <tojatos@gmail.com>
2017-04-10 17:48:09 +0200

    register view quickfix

--- application/views/Register/register.php
+++ application/views/Register/register.php
@@ -6,10 +6,10 @@
 	               		<h1 class="title">Zarejestruj się!</h1>
 	               		<hr />
 	               	</div>
-	            </div> 
+	            </div>
 				<div class="main-login main-center col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2
 							col-sm-8 col-sm-offset-2 col-sx-8 col-sx-offset-1">
-          <form class="register_form" method="post">						
+          <form class="register_form" method="post">
 						<div class="form-group">
               			<label for="login" class="cols-sm-2 control-label">Login</label>
 							<div class="cols-sm-10">
@@ -40,6 +40,15 @@
 							</div>
 						</div>
 
+						<div class="form-group">
+							<label for="password" class="cols-sm-2 control-label">Hasło</label>
+							<div class="cols-sm-10">
+								<div class="input-group">
+									<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+									<input type="password" class="form-control" name="password" id="password"  placeholder="Wpisz hasło"/>
+								</div>
+							</div>
+						</div>
 
 						<div class="form-group">
 							<label for="password_repeat" class="cols-sm-2 control-label">Powtórz hasło</label>
