1cbb8dda65d7ee2bc22475233b57624faa9ab89e
Yarde <dawidkal66@gmail.com>
2017-04-12 08:47:35 +0200

    kurs 2 + css

--- application/views/Kurs/kurs2.php
+++ application/views/Kurs/kurs2.php
@@ -1,7 +1,29 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
-Kurs 2
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<div class="container"><h1><u>Oparzenia</u></h1>
+<p><i>Oparzyłeśsię gotując obiad. Co robisz? Zastanawiasz się co zrobić, a może nie robisz nic? Ten materiał przybliży Ci podstawowe zasady udzielania pierwszej pomocy podczas oparzeń .</i></p>
+<div class="kurs_inner">
+<ol>
+<li><u class="pdk">Zadbaj o swoje bezpieczeństwo!</u></br> 
+	Jak zawsze, musisz zadbać o swoje bezpieczeń stwo. W przypadku oparzeń  ale i nie tylko, wskazane jest używanie rękawiczek ochronnych</li> 
+<img src="public/images/kurs.png" class="img-responsive" alt="Image"> 
+<li><u class="pdk">Wyeliminuj źródło ciepła lub czynnik parzący!</u></br>
+	Pierwszym, ważnym etapem udzielania pierwszej pomocy jest odcięcie poszkodowanego od źródła ciepła – czynnika parzącego. Często zdarza się, że poszkodowany sam – odruchem naturalnym, byłw stanie odciąć czynnik parzący.</li> 
+<img src="public/images/kurs.png" class="img-responsive" alt="Image"> 
+<li><u class="pdk">Chłodzenie poparzonego miejsca!</u></br>
+	</br>Teraz przychodzi czas na chłodzenie poparzonego miejsca czystą, zimną, najlepiej bieżącą wodąprzez około 15 minut dużą powierzchnię lub do ustąpienia bólu w przypadku małej powierzchni. Im szybciej zaczniemy chłodzić poparzone miejsce, tym lepiej ponieważchłodzenie skraca czas przegrzania – zmniejsza głębokość oparzenia. Podczas chłodzenia należy uważać by nie doprowadzić do nadmiernego ochłodzenia organizmu. Trzeba również zwracać uwagę na różnego rodzaju biżuterię– trzeba ją zdjąć.
+	<img src="public/images/kurs.png" class="img-responsive" alt="Image">
+<li><u class="pdk">Opatrunek</u></br>
+	Kolejnym krokiem jest osłonięcie rany jałowym opatrunkiem. Najpierw należy ranę okryć gazikami lub gazą w zależności od rozległości oparzenia, następnie gazę mocujemy przy pomocy bandaża. Trzeba zwrócić uwagę na to, aby opatrunek nie uwierałzbyt mocno na oparzone miejsce.</li>  
+<img src="public/images/kurs.png" class="img-responsive" alt="Image"> 
+<li><u class="pdk">Co dalej?</u></br>
+	W przypadku gdy stwierdzimy że poszkodowany jest nieprzytomny – bezwzględnie przechodzimy do sprawdzenia oddechu, a w razie potrzeby dalszej kontynuacji procedury RKO. Resuscytacja krążeniowo-oddechowa jest zdecydowanie ważniejsza niż opatrunek na ranie po oparzeniu.</br>
+	Jeśli stwierdzimy objawy wstrząsu musimy ułożyć poszkodowanego na plecach, z podniesionymi nogami. </br>
+	Jeśli oparzenia są rozległe, II/III stopnia, twarzy, szyi, klatki piersiowej czy krocza konieczna jest konsultacja lekarska.</br>
+	</li>
 <form class="finish_kurs_form" action="" method="post">
 	<input type="hidden" name="kurs_id" value="2">
 	<input type="submit" value="Zakończ kurs 2">
 </form>
+</div>
+
+
