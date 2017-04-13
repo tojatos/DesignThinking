1e36c6d4645b9d860e06ec153e7ff37c3dca3063
Yarde <dawidkal66@gmail.com>
2017-04-13 21:33:36 +0200

    kurs 3 i 4

--- application/views/Kurs/kurs3.php
+++ application/views/Kurs/kurs3.php
@@ -1,3 +1,37 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
-Kurs 3
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<div class="container"><h1><u>Omdlenia</u></h1>
+<p><i>
+    Siedząc w autobusie pan siedzący obok Ciebie mówi ci że ma zawroty głowy, a po chwili traci świadomość. Co zrobić? Powiedzieć kierowcy, a może samemu udzielić pierwszej pomocy? Ten materiał przybliży Ci podstawowe zasady udzielania pierwszej pomocy podczas omdleń.
+</i></p>
+<div class="kurs_inner">
+<ol>
+<li><u class="pdk">Zadbaj o swoje bezpieczeństwo!</u></br> 
+	Jak zawsze musisz czućsię bezpieczny żeby zacząć udzielać pierwszej pomocy osobie poszkodowanej. W przypadku omdleń  raczej nie będzie występowaćżadne zagrożenie dla pomagającego ale zawsze lepiej to sprawdzić.
+    </li> 
+<img src="public/images/kurs.png" class="img-responsive" alt="Image"> 
+<li><u class="pdk">Zapewnienie dostępu do powietrza! </u></br>
+	Wynieś poszkodowanego z miejsca w którym omdlał. Jeśli jest to niemożliwe zapewnij mu dostęp do świeżego powietrza.
+    </li> 
+<img src="public/images/kurs.png" class="img-responsive" alt="Image"> 
+<li><u class="pdk">Pozycja na wznak</u></br>
+	</br>Aby udzielenie pierwszej pomocy było łatwiejsze i wygodniejsze ułóż poszkodowanego na wznak – w pozycji leżącej na plecach.
+	<img src="public/images/kurs.png" class="img-responsive" alt="Image">
+<li><u class="pdk">Ocena funkcji życiowych! </u></br>
+	Udrożnij drogi oddechowe poszkodowanego i sprawdź czy prawidłowo oddycha (przez 10 sekund). Jeśli nie oddycha prawidłowo postępuj zgodnie z zasadami RKO, zaśjeśli oddycha przejdź do następnego kroku.
+    </li>  
+<img src="public/images/kurs.png" class="img-responsive" alt="Image"> 
+<li><u class="pdk">Uniesienie nóg poszkodowanego</u></br>
+	Unieś nogi poszkodowanego do góry aby zapewnić mu lepsze krążenie krwi, a co za tym idzie, szybsze dostarczenie tlenu do komórek.
+    </br>
+    <img src="public/images/kurs.png" class="img-responsive" alt="Image">
+    Po chwili powinna odzyskaćświadomość, jeśli tak się nie stanie masz do czynienia z utratąprzytomności i bezwzględnie powinieneś sprawdzić ponownie oddech, ułożyć poszkodowanego w pozycji bezpiecznej oraz wezwać pogotowie ratunkowe  (numer 999 lub 112). Nie zapominaj o dalszej kontroli oddechu, aż do przyjazdu zespołu ratownictwa medycznego.
+	</li>
+    <img src="public/images/kurs.png" class="img-responsive" alt="Image">
+</ol>
+<form class="finish_kurs_form" action="" method="post">
+	<input type="hidden" name="kurs_id" value="3">
+	<input type="submit" value="Zakończ kurs 3">
+</form>
+</div>
+
+
