93dc434589da58344840f0d747bcd40e9d89fa2a
Errancer <wincentypukar@gmail.com>
2017-04-17 22:00:36 +0200

    ranking model and table in progress, also PDF change

---
+++ application/views/Ranking/ranking.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<div class="container">
+    <p class="jumbotron p_rank">Ranking Ratowników</p>
+    <?= $data ?>
+</div>
+<?php //var_dump($data); ?>
+
