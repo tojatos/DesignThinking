3a67911f083d05503b00e3d91230165a23221d86
tojatos <tojatos@gmail.com>
2017-04-18 03:05:33 +0200

    User exam info + mainNav quickfix

--- application/views/inc/mainNav.php
+++ application/views/inc/mainNav.php
@@ -1,6 +1,5 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <nav class="navbar navbar-default">
-
   <div class="navbar-header">
     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
       <span class="sr-only">Toggle navigation</span>
@@ -10,9 +9,7 @@
     </button>
     <a class="navbar-brand" href="<?= site_url() ?>"><img src="<?= site_url() ?>public/images/logo.png" height="40px" width='200px'></a>
   </div>
-
   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
     <ul class="nav navbar-nav navbar-left">
       <li><a href="<?= site_url('Kurs')?>">Kursy</a></li>
       <?php if ($this->session->is_logged): ?>
@@ -20,13 +17,10 @@
       <?php endif; ?>
       <li><a href="<?= site_url('Ranking')?>">Ranking</a></li>
     </ul>
-
-
     <ul class="nav navbar-nav navbar-right">
       <?php if (!$this->session->is_logged): ?>
         <li><a href="<?= site_url('Register')?>">Zarejestruj się!</a></li>
         <li><a href="<?= site_url('Login')?>">Zaloguj się!</a></li>
-
       <?php else: ?>
         <span class="user_dropdown_toggle"><img src="" alt="Zdjęcie profilowe"><i class="fa fa-caret-down" aria-hidden="true"></i></span>
         <div class="user_dropdown">
@@ -43,9 +37,6 @@
               </form>
             </li>
           </ul>
-
-
-
         </div>
       <?php endif; ?>
     </ul>
