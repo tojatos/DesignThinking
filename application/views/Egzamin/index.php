ae74d449d0f13901171c993e1e0c7aad1cf0d88d
Yarde <dawidkal66@gmail.com>
2017-04-18 12:33:53 +0200

    egzamin css

--- application/views/Egzamin/index.php
+++ application/views/Egzamin/index.php
@@ -2,18 +2,18 @@
 <div class="container col-xs-12 col-sm-10 col-md-10 col-lg-9
 				 col-sm-offset-1 col-md-offset-2 col-lg-offset-3">
 				<div>
-			<a href="<?= site_url('Egzamin/1') ?>"><img src="public/images/Egzamin.png" class="img-responsive" alt="Image"></a>
+			<a href="<?= site_url('Egzamin/1') ?>"><img src="<?= site_url() ?>public/images/Egzamin.png" class="img-responsive" alt="Image"></a>
 		</div>
 						<div>
-			<a href="<?= site_url('Egzamin/2') ?>"><img src="public/images/Egzamin.png" class="img-responsive" alt="Image"></a>
+			<a href="<?= site_url('Egzamin/2') ?>"><img src="<?= site_url() ?>public/images/Egzamin.png" class="img-responsive" alt="Image"></a>
 		</div>
 						<div>
-			<a href="<?= site_url('Egzamin/3') ?>"><img src="public/images/Egzamin.png" class="img-responsive" alt="Image"></a>
+			<a href="<?= site_url('Egzamin/3') ?>"><img src="<?= site_url() ?>public/images/Egzamin.png" class="img-responsive" alt="Image"></a>
 		</div>
 						<div>
-			<a href="<?= site_url('Egzamin/4') ?>"><img src="public/images/Egzamin.png" class="img-responsive" alt="Image"></a>
+			<a href="<?= site_url('Egzamin/4') ?>"><img src="<?= site_url() ?>public/images/Egzamin.png" class="img-responsive" alt="Image"></a>
 		</div>
 				<div>
-			<a href="<?= site_url('Egzamin/5') ?>"><img src="public/images/Egzamin.png" class="img-responsive" alt="Image"></a>
+			<a href="<?= site_url('Egzamin/5') ?>"><img src="<?= site_url() ?>public/images/Egzamin.png" class="img-responsive" alt="Image"></a>
 		</div>
 </div>
