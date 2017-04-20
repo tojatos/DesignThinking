87f134d50b09587a5cc0c63e0009070ad55e303e
Yarde <dawidkal66@gmail.com>
2017-04-20 21:37:29 +0200

    kurs style

--- application/views/Egzamin/index.php
+++ application/views/Egzamin/index.php
@@ -1,30 +1,46 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <h1 class="p_rank">Egzaminy</h1>
-<div class="container col-xs-12 col-sm-10 col-md-10 col-lg-8
-				 col-sm-offset-1 col-md-offset-2 col-lg-offset-2 ">
+<div class="container">
+<div class="row">
+	<div class="col-xs-10 col-sm-10 col-md-8 col-lg-8 col-xs-offset-1 col-sm-offset-1 col-md-offset-2 col-lg-offset-2">
 	 <a href="<?= site_url('Egzamin/1') ?>">
 		<div class="frame_kurs1 transition_kurs">
 				<p class="name_kurs success new-label"><span class="align">Resuscytacja krążeniowo-oddechowa</span></p>
 		</div>
 	</a>
+	</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-xs-10 col-sm-10 col-md-5 col-lg-4 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1">
 	<a href="<?= site_url('Egzamin/2') ?>">
 		<div class="frame_kurs2 transition_kurs">
 				<p class="name_kurs success new-label"><span class="align">Oparzenia</span></p>
 		</div>
 	</a>
+	</div>
+	<div class="col-xs-10 col-sm-10 col-md-5 col-lg-4 col-xs-offset-1 col-sm-offset-1 col-md-offset-0 col-lg-offset-2">
 	<a href="<?= site_url('Egzamin/3') ?>">
 		<div class="frame_kurs3 transition_kurs">
 				<p class="name_kurs success new-label"><span class="align">Omdlenia</span></p>
 		</div>
 	</a>
+	</div>
+</div>
+<div class="row">
+	<div class="col-xs-10 col-sm-10 col-md-5 col-lg-4 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1">
 	<a href="<?= site_url('Egzamin/4') ?>">
 		<div class="frame_kurs4 transition_kurs">
 				<p class="name_kurs success new-label"><span class="align">Postępowanie z AED</span></p>
 		</div>
 	</a>
+	</div>
+	<div class="col-xs-10 col-sm-10 col-md-5 col-lg-4 col-xs-offset-1 col-sm-offset-1 col-md-offset-0 col-lg-offset-2">
 	<a href="<?= site_url('Egzamin/5') ?>">
 		<div class="frame_kurs5 transition_kurs">
 				<p class="name_kurs success new-label"><span class="align">Zadławienia</span></p>
 		</div>
 	</a>
+	</div>
+</div>
 </div>
--- application/views/Kurs/index.php
+++ application/views/Kurs/index.php
@@ -1,32 +1,48 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <h1 class="p_rank">Kursy</h1>
-<div class="container col-xs-12 col-sm-10 col-md-8 col-lg-8
-				 col-sm-offset-1 col-md-offset-2 col-lg-offset-2 ">
+<div class="container">
+<div class="row">
+	<div class="col-xs-10 col-sm-10 col-md-8 col-lg-8 col-xs-offset-1 col-sm-offset-1 col-md-offset-2 col-lg-offset-2">
 	 <a href="<?= site_url('Kurs/1') ?>">
 		<div class="frame_kurs1 transition_kurs">
 				<p class="name_kurs success new-label"><span class="align">Resuscytacja krążeniowo-oddechowa</span></p>
 		</div>
 	</a>
+	</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-xs-10 col-sm-10 col-md-5 col-lg-4 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1">
 	<a href="<?= site_url('Kurs/2') ?>">
 		<div class="frame_kurs2 transition_kurs">
 				<p class="name_kurs success new-label"><span class="align">Oparzenia</span></p>
 		</div>
 	</a>
+	</div>
+	<div class="col-xs-10 col-sm-10 col-md-5 col-lg-4 col-xs-offset-1 col-sm-offset-1 col-md-offset-0 col-lg-offset-2">
 	<a href="<?= site_url('Kurs/3') ?>">
 		<div class="frame_kurs3 transition_kurs">
 				<p class="name_kurs success new-label"><span class="align">Omdlenia</span></p>
 		</div>
 	</a>
+	</div>
+</div>
+<div class="row">
+	<div class="col-xs-10 col-sm-10 col-md-5 col-lg-4 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1">
 	<a href="<?= site_url('Kurs/4') ?>">
 		<div class="frame_kurs4 transition_kurs">
 				<p class="name_kurs success new-label"><span class="align">Postępowanie z AED</span></p>
 		</div>
 	</a>
+	</div>
+	<div class="col-xs-10 col-sm-10 col-md-5 col-lg-4 col-xs-offset-1 col-sm-offset-1 col-md-offset-0 col-lg-offset-2">
 	<a href="<?= site_url('Kurs/5') ?>">
 		<div class="frame_kurs5 transition_kurs">
 				<p class="name_kurs success new-label"><span class="align">Zadławienia</span></p>
 		</div>
 	</a>
+	</div>
+</div>
 </div>
 
 
