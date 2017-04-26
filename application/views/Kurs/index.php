80c640093dbd6d29f4fd28e9e5b14544352d526e
Yarde <dawidkal66@gmail.com>
2017-04-26 21:46:29 +0200

    navbar border

--- application/views/Kurs/index.php
+++ application/views/Kurs/index.php
@@ -3,41 +3,41 @@
 <div class="container">
 	<div class="row">
 		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-4 col-xs-offset-3 col-sm-offset-0 col-md-offset-0 col-lg-offset-0">
-			<div class="frame_kurs transition_kurs">
-				<a href="<?= site_url('Kurs/1') ?>">
+			<a href="<?= site_url('Kurs/1') ?>">
+				<div class="frame_kurs frame1 transition_kurs">
 					<p class="name_kurs success new-label"><span class="align">Kurs 1.</span></p>
-				</a>
-			</div>
+				</div>
+			</a>
 		</div>
 		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-4 col-xs-offset-3 col-sm-offset-0 col-md-offset-1 col-lg-offset-0">
-			<div class="frame_kurs transition_kurs">
-				<a href="<?= site_url('Kurs/2') ?>">
+			<a href="<?= site_url('Kurs/2') ?>">
+				<div class="frame_kurs transition_kurs">
 					<p class="name_kurs success new-label"><span class="align">Kurs 2.</span></p>
-				</a>
-			</div>
+				</div>
+			</a>
 		</div>
 		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-4 col-xs-offset-3 col-sm-offset-0 col-md-offset-1 col-lg-offset-0">
-			<div class="frame_kurs transition_kurs">
-				<a href="<?= site_url('Kurs/3') ?>">
+			<a href="<?= site_url('Kurs/3') ?>">
+				<div class="frame_kurs transition_kurs">
 					<p class="name_kurs success new-label"><span class="align">Kurs 3.</span></p>
-				</a>
-			</div>
+				</div>
+			</a>
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-xs-6 col-sm-5 col-md-3 col-lg-3 col-xs-offset-3 col-sm-offset-1 col-md-offset-2 col-lg-offset-2">
-			<div class="frame_kurs transition_kurs">
-				<a href="<?= site_url('Kurs/4') ?>">
+			<a href="<?= site_url('Kurs/4') ?>">
+				<div class="frame_kurs transition_kurs">
 					<p class="name_kurs success new-label"><span class="align">Kurs 4.</span></p>
-				</a>
-			</div>
+				</div>
+			</a>
 		</div>
 		<div class="col-xs-6 col-sm-5 col-md-3 col-lg-3 col-xs-offset-3 col-sm-offset-1 col-md-offset-2 col-lg-offset-1">
-			<div class="frame_kurs transition_kurs">
-				<a href="<?= site_url('Kurs/5') ?>">
+			<a href="<?= site_url('Kurs/5') ?>">
+				<div class="frame_kurs transition_kurs">
 					<p class="name_kurs success new-label"><span class="align">Kurs 5.</span></p>
-				</a>
-			</div>
+				</div>
+			</a>
 		</div>
 	</div>
 	<div class="row">
@@ -46,3 +46,11 @@
 		</div>
 	</div>
 </div>
+
+
+<script> 
+$('.frame1').mouseenter(function(){  stare = $(this).html();  
+  $(this).html("<p class='name_kurs success new-label'><span class='align'> //ma być wynik tego czy kurs ukończony</span></p>");   }) 
+ 
+$('.frame1').mouseleave(function(){  $(this).html(stare);   }) 
+</script>
