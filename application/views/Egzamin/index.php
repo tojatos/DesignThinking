9461617edb5e4edea06ec322ead5c96538340466
Yarde <dawidkal66@gmail.com>
2017-04-29 12:23:34 +0200

    different kurs layout

--- application/views/Egzamin/index.php
+++ application/views/Egzamin/index.php
@@ -2,44 +2,49 @@
 <h1 class="p_rank">Egzaminy</h1>
 <div class="container">
 	<div class="row">
-		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-4 col-xs-offset-3 col-sm-offset-0 col-md-offset-0 col-lg-offset-0">
-			<div class="frame_kurs transition_kurs">
+			<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xs-offset-2 col-sm-offset-0 col-md-offset-0 col-lg-offset-1">
 				<a href="<?= site_url('Egzamin/1') ?>">
-					<p class="name_kurs success new-label"><span class="align">Egzamin 1.</span></p>
+					<div class="frame_kurs transition_kurs">
+						<p class="name_kurs success new-label"><span class="align">Egzamin 1.</span></p>
+					</div>
 				</a>
 			</div>
-		</div>
-		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-4 col-xs-offset-3 col-sm-offset-0 col-md-offset-0 col-lg-offset-0">
-			<div class="frame_kurs transition_kurs">
+			<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xs-offset-2 col-sm-offset-0 col-md-offset-0 col-lg-offset-0">
 				<a href="<?= site_url('Egzamin/2') ?>">
-					<p class="name_kurs success new-label"><span class="align">Egzamin 2.</span></p>
+					<div class="frame_kurs transition_kurs">
+						<p class="name_kurs success new-label"><span class="align">Egzamin 2.</span></p>
+					</div>
 				</a>
 			</div>
-		</div>
-		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-4 col-xs-offset-3 col-sm-offset-0 col-md-offset-0 col-lg-offset-0">
-			<div class="frame_kurs transition_kurs">
+			<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xs-offset-2 col-sm-offset-0 col-md-offset-0 col-lg-offset-0">
 				<a href="<?= site_url('Egzamin/3') ?>">
-					<p class="name_kurs success new-label"><span class="align">Egzamin 3.</span></p>
+					<div class="frame_kurs transition_kurs">
+						<p class="name_kurs success new-label"><span class="align">Egzamin 3.</span></p>
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xs-offset-2 col-sm-offset-0 col-md-offset-0 col-lg-offset-0">
+				<a href="<?= site_url('Egzamin/4') ?>">
+					<div class="frame_kurs transition_kurs">
+						<p class="name_kurs success new-label"><span class="align">Egzamin 4.</span></p>
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-6 col-sm-5 col-md-6 col-lg-2 col-xs-offset-2 col-sm-offset-1 col-md-offset-0 col-lg-offset-0">
+				<a href="<?= site_url('Egzamin/5') ?>">
+					<div class="frame_kurs transition_kurs">
+						<p class="name_kurs success new-label"><span class="align">Egzamin 5.</span></p>
+					</div>
 				</a>
 			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-xs-6 col-sm-5 col-md-3 col-lg-3 col-xs-offset-3 col-sm-offset-1 col-md-offset-2 col-lg-offset-2">
-				<div class="frame_kurs transition_kurs">
-			<a href="<?= site_url('Egzamin/4') ?>">
 
-						<p class="name_kurs success new-label"><span class="align">Egzamin 4.</span></p>
-
-			</a>
-			</div>
-			</div>
-			<div class="col-xs-6 col-sm-5 col-md-3 col-lg-3 col-xs-offset-3 col-sm-offset-1 col-md-offset-2 col-lg-offset-1">
-				<div class="frame_kurs transition_kurs">
-					<a href="<?= site_url('Egzamin/5') ?>">
-						<p class="name_kurs success new-label"><span class="align">Egzamin 5.</span></p>
-					</a>
-				</div>
-			</div>
-		</div>
-	</div>
+<!--$color = zielone lub czerwone z zależności czy zdane czy nie-->
+<!--$info = informacja czy zdane czy nie (w egzaminach dodatkowo %)-->
+<script> 
+$('.frame1').mouseenter(function(){  stare = $(this).html();  
+  $(this).html("<p style='background-color:$color;' class='name_kurs success new-label'><span class='align'> $info</span></p>");   }) 
+ 
+$('.frame1').mouseleave(function(){  $(this).html(stare);   }) 
+</script>
--- application/views/Kurs/index.php
+++ application/views/Kurs/index.php
@@ -2,37 +2,35 @@
 <h1 class="p_rank">Kursy</h1>
 <div class="container">
 	<div class="row">
-		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-4 col-xs-offset-3 col-sm-offset-0 col-md-offset-0 col-lg-offset-0">
+		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xs-offset-2 col-sm-offset-0 col-md-offset-0 col-lg-offset-1">
 			<a href="<?= site_url('Kurs/1') ?>">
 				<div class="frame_kurs frame1 transition_kurs">
 					<p class="name_kurs success new-label"><span class="align">Kurs 1.</span></p>
 				</div>
 			</a>
 		</div>
-		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-4 col-xs-offset-3 col-sm-offset-0 col-md-offset-1 col-lg-offset-0">
+		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xs-offset-2 col-sm-offset-0 col-md-offset-0 col-lg-offset-0">
 			<a href="<?= site_url('Kurs/2') ?>">
 				<div class="frame_kurs transition_kurs">
 					<p class="name_kurs success new-label"><span class="align">Kurs 2.</span></p>
 				</div>
 			</a>
 		</div>
-		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-4 col-xs-offset-3 col-sm-offset-0 col-md-offset-1 col-lg-offset-0">
+		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xs-offset-2 col-sm-offset-0 col-md-offset-0 col-lg-offset-0">
 			<a href="<?= site_url('Kurs/3') ?>">
 				<div class="frame_kurs transition_kurs">
 					<p class="name_kurs success new-label"><span class="align">Kurs 3.</span></p>
 				</div>
 			</a>
 		</div>
-	</div>
-	<div class="row">
-		<div class="col-xs-6 col-sm-5 col-md-3 col-lg-3 col-xs-offset-3 col-sm-offset-1 col-md-offset-2 col-lg-offset-2">
+		<div class="col-xs-6 col-sm-5 col-md-3 col-lg-2 col-xs-offset-2 col-sm-offset-1 col-md-offset-0 col-lg-offset-0">
 			<a href="<?= site_url('Kurs/4') ?>">
 				<div class="frame_kurs transition_kurs">
 					<p class="name_kurs success new-label"><span class="align">Kurs 4.</span></p>
 				</div>
 			</a>
 		</div>
-		<div class="col-xs-6 col-sm-5 col-md-3 col-lg-3 col-xs-offset-3 col-sm-offset-1 col-md-offset-2 col-lg-offset-1">
+		<div class="col-xs-6 col-sm-5 col-md-6 col-lg-2 col-xs-offset-2 col-sm-offset-1 col-md-offset-0 col-lg-offset-0">
 			<a href="<?= site_url('Kurs/5') ?>">
 				<div class="frame_kurs transition_kurs">
 					<p class="name_kurs success new-label"><span class="align">Kurs 5.</span></p>
@@ -47,10 +45,11 @@
 	</div>
 </div>
 
-
+<!--$color = zielone lub czerwone z zależności czy zdane czy nie-->
+<!--$info = informacja czy zdane czy nie (w egzaminach dodatkowo %)-->
 <script> 
 $('.frame1').mouseenter(function(){  stare = $(this).html();  
-  $(this).html("<p class='name_kurs success new-label'><span class='align'> //ma być wynik tego czy kurs ukończony</span></p>");   }) 
+  $(this).html("<p style='background-color:$color;' class='name_kurs success new-label'><span class='align'> $info</span></p>");   }) 
  
 $('.frame1').mouseleave(function(){  $(this).html(stare);   }) 
 </script>
