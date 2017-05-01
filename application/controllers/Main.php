72a708a4c4a9744448665d3c9e68dc5d9341c1b0
tojatos <tojatos@gmail.com>
2017-05-01 23:19:06 +0200

    Main - pobieranie karty młodego ratownika

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -5,14 +5,24 @@ class Main extends MY_Controller
 {
     public function index()
     {
-        $data['title'] = 'Design Thinking';
-        $data['mainNav'] = $this->loadMainNav();
-        $data['content'] = $this->loadContent('main');
-        $this->showMainView($data);
+        $this->load->model('Egzamin_model');
+        $this->load->model('User_model');
+
+        if ($this->session->is_logged) {
+            $user_name = $this->session->user_name;
+            $user_id = $this->User_model->get_user_id($user_name);
+            $finished_exams_number = $this->Egzamin_model->get_finished_exams_number($user_id);
+
+            $view['content'] = $this->loadContent('main', ['finished_exams_number' => $finished_exams_number]);
+        } else {
+            $view['content'] = $this->loadContent('main');
+        }
+
+        $view['mainNav'] = $this->loadMainNav();
+        $this->showMainView($view);
     }
     public function error404()
     {
         $this->showView('404');
     }
-
 }
