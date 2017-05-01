9eef51d641cb24cc206f65c5b33bc3c7eddc8cc7
Yarde <dawidkal66@gmail.com>
2017-05-01 23:28:56 +0200

    egzamin finish display

--- application/views/Egzamin/index.php
+++ application/views/Egzamin/index.php
@@ -3,47 +3,52 @@
 <div class="container">
 	<div class="row col-lg-offset-1 col-md-offset-1">
 			<div class="col-xs-6 col-sm-4 col-md-3 col-lg-4 col-xs-offset-2 col-sm-offset-0 col-md-offset-1 col-lg-offset-0">
-				<?= ($exam_results[1] != null) ? 'Wynik:'.$exam_results[1].'%' : ''  ?> <br>
-				<?= ($exam_results[1] != null && $exam_results[1] >= TRESHOLD) ? 'Zdany' : 'Nie zdany' ?>
 				<a href="<?= site_url('Egzamin/1') ?>">
 					<div class="frame_kurs transition_kurs">
-						<p class="name_kurs success new-label"><span class="align">Egzamin 1.</span></p>
+						<p class="name_kurs success new-label"><span class="align">Egzamin 1.</span>
+						<?= ($exam_results[1] != null && $exam_results[1] >= TRESHOLD) ? 
+						"<span class='status_t'>zdany":"<span class='status_f'>nie zdany" ?>
+						<?= ($exam_results[1] != null) ? $exam_results[1].'%</span>' : '</span>'  ?></p>
 					</div>
 				</a>
 			</div>
 			<div class="col-xs-6 col-sm-4 col-md-3 col-lg-4 col-xs-offset-2 col-sm-offset-0 col-md-offset-0 col-lg-offset-0">
-				<?= ($exam_results[2] != null) ? 'Wynik:'.$exam_results[2].'%' : ''  ?> <br>
-				<?= ($exam_results[2] != null && $exam_results[2] >= TRESHOLD) ? 'Zdany' : 'Nie zdany' ?>
 				<a href="<?= site_url('Egzamin/2') ?>">
 					<div class="frame_kurs transition_kurs">
-						<p class="name_kurs success new-label"><span class="align">Egzamin 2.</span></p>
+						<p class="name_kurs success new-label"><span class="align">Egzamin 1.</span>
+						<?= ($exam_results[2] != null && $exam_results[2] >= TRESHOLD) ? 
+						"<span class='status_t'>zdany":"<span class='status_f'>nie zdany" ?>
+						<?= ($exam_results[2] != null) ? $exam_results[2].'%</span>' : '</span>'  ?></p>
 					</div>
 				</a>
 			</div>
 			<div class="col-xs-6 col-sm-4 col-md-3 col-lg-4 col-xs-offset-2 col-sm-offset-0 col-md-offset-0 col-lg-offset-0">
-				<?= ($exam_results[3] != null) ? 'Wynik:'.$exam_results[3].'%' : ''  ?> <br>
-				<?= ($exam_results[3] != null && $exam_results[3] >= TRESHOLD) ? 'Zdany' : 'Nie zdany' ?>
 				<a href="<?= site_url('Egzamin/3') ?>">
 					<div class="frame_kurs transition_kurs">
-						<p class="name_kurs success new-label"><span class="align">Egzamin 3.</span></p>
+						<p class="name_kurs success new-label"><span class="align">Egzamin 1.</span>
+						<?= ($exam_results[3] != null && $exam_results[3] >= TRESHOLD) ? 
+						"<span class='status_t'>zdany":"<span class='status_f'>nie zdany" ?>
+						<?= ($exam_results[3] != null) ? $exam_results[3].'%</span>' : '</span>'  ?></p>
 					</div>
 				</a>
 			</div>
 			<div class="col-xs-6 col-sm-5 col-md-3 col-lg-3 col-xs-offset-2 col-sm-offset-1 col-md-offset-2 col-lg-offset-2">
-				<?= ($exam_results[4] != null) ? 'Wynik:'.$exam_results[4].'%' : ''  ?> <br>
-				<?= ($exam_results[4] != null && $exam_results[4] >= TRESHOLD) ? 'Zdany' : 'Nie zdany' ?>
 				<a href="<?= site_url('Egzamin/4') ?>">
 					<div class="frame_kurs transition_kurs">
-						<p class="name_kurs success new-label"><span class="align">Egzamin 4.</span></p>
+						<p class="name_kurs success new-label"><span class="align">Egzamin 1.</span>
+						<?= ($exam_results[4] != null && $exam_results[4] >= TRESHOLD) ? 
+						"<span class='status_t'>zdany":"<span class='status_f'>nie zdany" ?>
+						<?= ($exam_results[4] != null) ? $exam_results[4].'%</span>' : '</span>'  ?></p>
 					</div>
 				</a>
 			</div>
 			<div class="col-xs-6 col-sm-5 col-md-3 col-lg-3 col-xs-offset-2 col-sm-offset-1 col-md-offset-1 col-lg-offset-1">
-				<?= ($exam_results[5] != null) ? 'Wynik:'.$exam_results[5].'%' : ''  ?> <br>
-				<?= ($exam_results[5] != null && $exam_results[5] >= TRESHOLD) ? 'Zdany' : 'Nie zdany' ?>
 				<a href="<?= site_url('Egzamin/5') ?>">
 					<div class="frame_kurs transition_kurs">
-						<p class="name_kurs success new-label"><span class="align">Egzamin 5.</span></p>
+						<p class="name_kurs success new-label"><span class="align">Egzamin 1.</span>
+						<?= ($exam_results[5] != null && $exam_results[5] >= TRESHOLD) ? 
+						"<span class='status_t'>zdany":"<span class='status_f'>nie zdany" ?>
+						<?= ($exam_results[5] != null) ? $exam_results[5].'%</span>' : '</span>'  ?></p>
 					</div>
 				</a>
 			</div>
