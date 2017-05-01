67dbcc84baef63f89753d29af318b821bb077e51
tojatos <tojatos@gmail.com>
2017-05-02 00:08:52 +0200

    Main - treshold info

--- application/views/main.php
+++ application/views/main.php
@@ -4,6 +4,7 @@
 				<h1>Witaj młody ratowniku</h1>
 				<p>Dzięki tej stronie możesz dowiedzieć się więcej na temat ratowania ludzkiego życia</p>
 				<p>Możesz ukończyć kursy, a następnie sprawdzić się w egzaminach</p>
+				<p>Aby zdać egzamin, musisz go zdać na minimum <?= TRESHOLD ?>%. Powodzenia!</p>
 				<p><a class="btn btn-primary btn-lg" href="<?= site_url('Kurs') ?>" role="button">Przejdź do kursów</a></p>
 		</div>
 		<div class="jumbotron">
