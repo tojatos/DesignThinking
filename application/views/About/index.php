6a45006c1995fd03499bea17f7c76e0e3f2975c1
Yarde <dawidkal66@gmail.com>
2017-05-03 17:07:40 +0200

    about fiilmik

--- application/views/About/index.php
+++ application/views/About/index.php
@@ -1,1 +1,6 @@
-<div class="btn btn-danger">O nas - testowy przycisk</div>
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<div class="container">
+    <video controls>
+    <source src="<?= site_url() ?>public/images/filmik.mp4" type="video/mp4">
+    </video>
+</div>
