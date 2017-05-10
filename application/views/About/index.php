34d9d3f556ce82b4e1e9581b31a54bb160dedf80
Yarde <dawidkal66@gmail.com>
2017-05-10 10:54:18 +0200

    video fix

--- application/views/About/index.php
+++ application/views/About/index.php
@@ -1,6 +1,4 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <div class="container">
-    <video controls>
-    <source src="<?= site_url() ?>public/images/filmik.mp4" type="video/mp4">
-    </video>
+    <iframe src="https://player.vimeo.com/video/216678979?color=2086bf&byline=0&portrait=0" class="video" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 </div>
