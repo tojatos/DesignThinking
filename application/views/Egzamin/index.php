cf45f66a447ff8fa7c43a43a4646d389999f7e2b
Yarde <dawidkal66@gmail.com>
2017-05-10 11:23:46 +0200

    navbar validation

--- application/views/Egzamin/index.php
+++ application/views/Egzamin/index.php
@@ -1,8 +1,8 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <h1 class="p_rank">Egzaminy</h1>
 <div class="container">
-	<div class="row">
-			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xs-offset-1 col-sm-offset-0 col-md-offset-2 col-lg-offset-0">
+	<div class="row  col-xs-12">
+			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-sm-offset-0 col-md-offset-2 col-lg-offset-0">
 				<a href="<?= site_url('Egzamin/1') ?>">
 					<div class="frame_kurs transition_kurs">
 						<p class="name_kurs success new-label"><span class="align">1. Resuscytacja krążeniowo-oddechowa</span></br>
@@ -12,7 +12,7 @@
 					</div>
 				</a>
 			</div>
-			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xs-offset-1 col-sm-offset-0 col-md-offset-0 col-lg-offset-0">
+			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-sm-offset-0 col-md-offset-0 col-lg-offset-0">
 				<a href="<?= site_url('Egzamin/2') ?>">
 					<div class="frame_kurs transition_kurs">
 						<p class="name_kurs success new-label"><span class="align">2. Oparzenia</span></br>
@@ -22,7 +22,7 @@
 					</div>
 				</a>
 			</div>
-			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xs-offset-1 col-sm-offset-0 col-md-offset-4 col-lg-offset-0">
+			<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-sm-offset-0 col-md-offset-4 col-lg-offset-0">
 				<a href="<?= site_url('Egzamin/3') ?>">
 					<div class="frame_kurs transition_kurs">
 						<p class="name_kurs success new-label"><span class="align">3. Omdlenia</span></br>
@@ -32,7 +32,7 @@
 					</div>
 				</a>
 			</div>
-			<div class="col-xs-12 col-sm-5 col-md-3 col-lg-3 col-xs-offset-1 col-sm-offset-0 col-md-offset-2 col-lg-offset-2">
+			<div class="col-xs-12 col-sm-5 col-md-3 col-lg-3 col-sm-offset-0 col-md-offset-2 col-lg-offset-2">
 				<a href="<?= site_url('Egzamin/4') ?>">
 					<div class="frame_kurs transition_kurs">
 						<p class="name_kurs success new-label"><span class="align">4. Postępowanie z AED</span></br>
@@ -42,7 +42,7 @@
 					</div>
 				</a>
 			</div>
-			<div class="col-xs-6 col-sm-5 col-md-3 col-lg-3 col-xs-offset-1 col-sm-offset-3 col-md-offset-1 col-lg-offset-1">
+			<div class="col-xs-6 col-sm-5 col-md-3 col-lg-3 col-sm-offset-3 col-md-offset-1 col-lg-offset-1">
 				<a href="<?= site_url('Egzamin/5') ?>">
 					<div class="frame_kurs transition_kurs">
 						<p class="name_kurs success new-label"><span class="align">5. Zadławienia</span></br>
--- application/views/inc/mainNav.php
+++ application/views/inc/mainNav.php
@@ -7,15 +7,15 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="<?= site_url() ?>"><img src="<?= site_url() ?>public/images/logo.png" height="40px" width='200px'></a>
+    <a class="navbar-brand" href="<?= site_url() ?>"><img src="<?= site_url() ?>public/images/logo.png" height="40px" width='200px' alt=""/></a>
   </div>
   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
     <ul class="nav navbar-nav navbar-left">
-      <li><a href="<?= site_url('Kurs')?>"class="<?= ($this->uri->segment(1)=="Kurs") ? "active" : ""?>">Kursy</a></li>
+      <li><a href="<?= site_url('Kurs')?>" class="<?= ($this->uri->segment(1)=="Kurs") ? "active" : ""?>">Kursy</a></li>
       <?php if ($this->session->is_logged): ?>
-        <li><a href="<?= site_url('Egzamin')?>"class="<?= ($this->uri->segment(1)=="Egzamin") ? "active" : ""?>">Egzaminy</a></li>
+        <li><a href="<?= site_url('Egzamin')?>" class="<?= ($this->uri->segment(1)=="Egzamin") ? "active" : ""?>">Egzaminy</a></li>
       <?php endif; ?>
-      <li><a href="<?= site_url('Ranking')?>"class="<?= ($this->uri->segment(1)=="Ranking") ? "active" : ""?>">Ranking</a></li>
+      <li><a href="<?= site_url('Ranking')?>" class="<?= ($this->uri->segment(1)=="Ranking") ? "active" : ""?>">Ranking</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
       <li><a href="<?= site_url('About')?>"class="<?= ($this->uri->segment(1)=="About") ? "active" : ""?>">O projekcie</a></li>
